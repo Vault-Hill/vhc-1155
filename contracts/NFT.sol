bb2ac511152219d24ef163c98c5e9ebdfa658026
Yogeshwar Parth <yogeshwarp@chetu.com>
2022-05-13 17:29:21 +0530

    contact

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -5,34 +5,27 @@ import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract NFT is ERC1155, Ownable  {
-
     
-    // Base URI
-    string private baseURI;
-    string public name;
     string public dataURl ;
 
-    constructor (
-     string memory url
-        ) 
-        ERC1155(url){
-            setName('vaultHill');
-            dataURl = url;
-        }
-    
-    function  setName(string memory _name) public onlyOwner{
-        name =_name;
-    }
+    constructor (string memory url) ERC1155(url){ dataURl = url; }
 
+    function setURI(string memory newuri) public onlyOwner {
+        _setURI(newuri);
+    }
+    
     function mint(uint256 id, uint256 amount, string memory _newuri) public {
-         require(id != 0 , "Token does not exist !");
-          _setURI(_newuri);
+
+        require(id != 0 , "Token does not exist !");
+        _setURI(_newuri);
         _mint(msg.sender, id, amount, "");
+        
     }
 
     function mintBatch(uint256[] memory ids, uint256[] memory amounts) public {
       
         _mintBatch(msg.sender, ids, amounts, "");
+
     }
 
 }
