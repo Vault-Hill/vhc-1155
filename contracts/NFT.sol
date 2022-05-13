e71c0dce17375aeeac09da22573d8597519673e8
Yogeshwar Parth <yogeshwarp@chetu.com>
2022-05-13 15:08:02 +0530

    remove nft list and purchase

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -6,14 +6,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract NFT is ERC1155, Ownable  {
 
-    mapping(address => mapping(uint256 =>Listing)) public listings;
-    mapping(address => uint256) public balances;
-
-    struct Listing{
-        uint price;
-        address seller;
-    }
-
+    
     // Base URI
     string private baseURI;
     string public name;
@@ -40,26 +33,6 @@ contract NFT is ERC1155, Ownable  {
     function mintBatch(uint256[] memory ids, uint256[] memory amounts) public {
       
         _mintBatch(msg.sender, ids, amounts, "");
-    }
-
-    function addListing(uint256 price, address contractAddr, uint tokenId) public {
-
-        ERC1155 token = ERC1155(contractAddr);
-        require(token.balanceOf(msg.sender, tokenId)>0 , "Caller must own the given token !");
-      
-        require(token.isApprovedForAll(msg.sender, address(this)), "Contract must approved");
-        listings[contractAddr][tokenId] = Listing(price, msg.sender);
-
-    }
-
-    function purchase(address contractAddr, uint256 tokenId, uint256 amount) public payable {
-
-        Listing memory item = listings[contractAddr][tokenId];
-        require(msg.value >= item.price * amount, "Insufficient fund sent");
-        balances[item.seller] += msg.value;
-        ERC1155 token = ERC1155(contractAddr); 
-        token.safeTransferFrom(item.seller, msg.sender, tokenId, amount, "");
-        
     }
 
 }
